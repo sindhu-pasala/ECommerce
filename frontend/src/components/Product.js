b50340f18a881de2c0a86d8d2915a1f55f25c4d6
sindhusha <sindhushapasala1999@gmail.com>
2022-11-20 22:45:42 +0530

    Home Screen and Rating Component

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -1,7 +1,29 @@
 import React from "react";
+import { Card } from "react-bootstrap";
+import Rating from "./Rating";
 
-const Product = () => {
-  return <div></div>;
+const Product = ({ product }) => {
+  return (
+    <Card className="my-3 p-3 rounded">
+      <a href={`/product/${product._id}`}>
+        <Card.Img src={product.image} variant="top" />
+      </a>
+
+      <Card.Body>
+        <a href={`/product/${product._id}`}>
+          <Card.Title as="div">
+            <strong>{product.name}</strong>
+          </Card.Title>
+        </a>
+      </Card.Body>
+
+      <Card.Text as="div">
+        <Rating value={product.rating} text={`${product.numReviews} reviews`} />
+      </Card.Text>
+
+      <Card.Text as="h3">${product.price}</Card.Text>
+    </Card>
+  );
 };
 
 export default Product;
